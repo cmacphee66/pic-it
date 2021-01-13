3f93b8161da854d71aacbd13b27d63354aabd029
Connor MacPhee <cmacphee66@gmail.com>
2021-01-13 13:39:29 +0000

    react-native/expo enviroment created

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,8 +7,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World!!</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
